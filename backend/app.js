055ac8d44cbb0bcdbf64475f1e340b2cd1098b0a
AryanNoSensie <officearyan14@gmail.com>
2025-06-24 06:35:37 +0530

    step 1 done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config( "../.env");
+import mongoose from 'mongoose';
+import connectDB from './src/config/mongo.config.js';
+
+
+
+
 const app = express();
+
+
 
 //getting nanoid from nanoid to make it smaller in chars 
 import { nanoid } from 'nanoid';
@@ -12,12 +22,28 @@ app.post('/api/create', (req, res) => {
     const {Url} = req.body;
     console.log(Url);
 
+
     res.send({nanoid: nanoid(7)});
 });
 
 
-app.listen(3000, () => {
+
+
+
+async function connection (){
+
+    try {
+       await connectDB();
+
+        app.listen(3000, () => {
     console.log('Server is running on port  on https://localhost:3000');
 }
 );
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
 
+}
+connection();
---
+++ backend/src/config/mongo.config.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+   const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log("mogodb connected");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    return process.exit(1);
+  }
+}     ;
+
+export default connectDB;
+
+
+
+const  db = moongos
